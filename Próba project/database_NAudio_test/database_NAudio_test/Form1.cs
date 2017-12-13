c698b1620997de191a3634175d869e749cac45d1
David Nagy <david075@freemail.hu>
2017-12-13 13:03:43 +0100

    test project hozzáadva / dokumentáció kibővítése

---
+++ Próba project/database_NAudio_test/database_NAudio_test/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace database_NAudio_test
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string mysqlConnection = "datasource=localhost;port=3306;username=root;password=asdfghjk123";
+            SqlConnection sqlcon = new SqlConnection();
+            sqlcon.Open();
+            textBox1.Text = "kapcsolódva";
+
+            SqlCommand command = new SqlCommand("SELECT * FROM [Users]", sqlcon);
+            SqlDataReader reader = command.ExecuteReader();
+            while (reader.Read()) {
+                richTextBox1.Text = "első tábla: " + reader.GetString(0);
+            }
+            reader.Close();
+            sqlcon.Close();
+        }
+
+
+        NAudio.Wave.WaveIn source = null;
+        NAudio.Wave.DirectSoundOut waveOut = null;
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            source = new NAudio.Wave.WaveIn();
+            source.DeviceNumber = 0;
+            source.WaveFormat = new NAudio.Wave.WaveFormat(44100, NAudio.Wave.WaveIn.GetCapabilities(0).Channels);
+
+            NAudio.Wave.WaveInProvider waveIn = new NAudio.Wave.WaveInProvider(source);
+
+            waveOut = new NAudio.Wave.DirectSoundOut();
+            waveOut.Init(waveIn);
+
+            textBox2.Text = "lejátszás folyamatos";
+
+            source.StartRecording();
+            waveOut.Play();
+        }
+    }
+}
--- Szakdolgozat_Project/sPeachVoice/sPeachVoice/mainForm.Designer.cs
+++ Szakdolgozat_Project/sPeachVoice/sPeachVoice/mainForm.Designer.cs
@@ -41,6 +41,7 @@
             this.servers_btn = new System.Windows.Forms.Button();
             this.conversation_btn = new System.Windows.Forms.Button();
             this.dashboard_panel = new System.Windows.Forms.Panel();
+            this.signout_btn = new System.Windows.Forms.Button();
             this.panel1.SuspendLayout();
             this.panel3.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
@@ -50,7 +51,7 @@
             // 
             // panel1
             // 
-            this.panel1.BackColor = System.Drawing.SystemColors.Highlight;
+            this.panel1.BackColor = System.Drawing.Color.LightSalmon;
             this.panel1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("panel1.BackgroundImage")));
             this.panel1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
             this.panel1.Controls.Add(this.panel2);
@@ -68,7 +69,7 @@
             // 
             // panel3
             // 
-            this.panel3.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.panel3.BackColor = System.Drawing.Color.LightSalmon;
             this.panel3.Controls.Add(this.pictureBox2);
             this.panel3.Controls.Add(this.label2);
             this.panel3.Controls.Add(this.label1);
@@ -80,6 +81,7 @@
             // 
             // pictureBox2
             // 
+            this.pictureBox2.BackColor = System.Drawing.Color.Chartreuse;
             this.pictureBox2.Location = new System.Drawing.Point(161, 54);
             this.pictureBox2.Name = "pictureBox2";
             this.pictureBox2.Size = new System.Drawing.Size(12, 12);
@@ -89,9 +91,10 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(105, 53);
+            this.label2.Font = new System.Drawing.Font("Lucida Sans", 8.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(94, 51);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(50, 13);
+            this.label2.Size = new System.Drawing.Size(61, 15);
             this.label2.TabIndex = 2;
             this.label2.Text = "Available";
             // 
@@ -116,6 +119,7 @@
             // panel4
             // 
             this.panel4.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.panel4.Controls.Add(this.signout_btn);
             this.panel4.Controls.Add(this.options_btn);
             this.panel4.Controls.Add(this.servers_btn);
             this.panel4.Controls.Add(this.conversation_btn);
@@ -126,54 +130,77 @@
             // 
             // options_btn
             // 
+            this.options_btn.BackColor = System.Drawing.Color.LightSalmon;
+            this.options_btn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.options_btn.Font = new System.Drawing.Font("Lucida Sans", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
             this.options_btn.Location = new System.Drawing.Point(0, 120);
             this.options_btn.Name = "options_btn";
             this.options_btn.Size = new System.Drawing.Size(212, 63);
             this.options_btn.TabIndex = 2;
             this.options_btn.Text = "Options";
-            this.options_btn.UseVisualStyleBackColor = true;
+            this.options_btn.UseVisualStyleBackColor = false;
             this.options_btn.Click += new System.EventHandler(this.options_btn_Click);
             // 
             // servers_btn
             // 
+            this.servers_btn.BackColor = System.Drawing.Color.LightSalmon;
+            this.servers_btn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.servers_btn.Font = new System.Drawing.Font("Lucida Sans", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
             this.servers_btn.Location = new System.Drawing.Point(0, 60);
             this.servers_btn.Name = "servers_btn";
             this.servers_btn.Size = new System.Drawing.Size(212, 63);
             this.servers_btn.TabIndex = 1;
             this.servers_btn.Text = "Servers";
-            this.servers_btn.UseVisualStyleBackColor = true;
+            this.servers_btn.UseVisualStyleBackColor = false;
             this.servers_btn.Click += new System.EventHandler(this.servers_btn_Click);
             // 
             // conversation_btn
             // 
+            this.conversation_btn.BackColor = System.Drawing.Color.LightSalmon;
+            this.conversation_btn.Cursor = System.Windows.Forms.Cursors.Default;
+            this.conversation_btn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.conversation_btn.Font = new System.Drawing.Font("Lucida Sans", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
             this.conversation_btn.Location = new System.Drawing.Point(0, 0);
             this.conversation_btn.Name = "conversation_btn";
             this.conversation_btn.Size = new System.Drawing.Size(212, 63);
             this.conversation_btn.TabIndex = 0;
             this.conversation_btn.Text = "Conversations";
-            this.conversation_btn.UseVisualStyleBackColor = true;
+            this.conversation_btn.UseVisualStyleBackColor = false;
             this.conversation_btn.Click += new System.EventHandler(this.conversation_btn_Click);
             // 
             // dashboard_panel
             // 
-            this.dashboard_panel.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.dashboard_panel.BackColor = System.Drawing.Color.Coral;
             this.dashboard_panel.Location = new System.Drawing.Point(218, 0);
             this.dashboard_panel.Name = "dashboard_panel";
-            this.dashboard_panel.Size = new System.Drawing.Size(458, 478);
+            this.dashboard_panel.Size = new System.Drawing.Size(458, 453);
             this.dashboard_panel.TabIndex = 3;
+            // 
+            // signout_btn
+            // 
+            this.signout_btn.BackColor = System.Drawing.Color.LightSalmon;
+            this.signout_btn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.signout_btn.Font = new System.Drawing.Font("Lucida Sans", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.signout_btn.Location = new System.Drawing.Point(0, 180);
+            this.signout_btn.Name = "signout_btn";
+            this.signout_btn.Size = new System.Drawing.Size(212, 63);
+            this.signout_btn.TabIndex = 3;
+            this.signout_btn.Text = "Sign Out";
+            this.signout_btn.UseVisualStyleBackColor = false;
             // 
             // mainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(677, 478);
+            this.BackColor = System.Drawing.Color.LightSalmon;
+            this.ClientSize = new System.Drawing.Size(677, 452);
             this.Controls.Add(this.dashboard_panel);
             this.Controls.Add(this.panel4);
             this.Controls.Add(this.panel3);
             this.Controls.Add(this.panel1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "mainForm";
-            this.Text = "mainForm";
+            this.Text = "sPeach";
             this.panel1.ResumeLayout(false);
             this.panel3.ResumeLayout(false);
             this.panel3.PerformLayout();
@@ -198,5 +225,6 @@
         private System.Windows.Forms.Button options_btn;
         private System.Windows.Forms.Button servers_btn;
         private System.Windows.Forms.Button conversation_btn;
+        private System.Windows.Forms.Button signout_btn;
     }
 }
